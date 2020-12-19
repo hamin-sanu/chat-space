094dbdd64b4c06e42517d071d6c296cc407d1635
hamin-sanu <75008521+hamin-sanu@users.noreply.github.com>
2020-12-19 12:58:06 +0900

    非同期通信の実装

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,66 @@
+$(function(){
+  function buildHTML(message){
+    if ( message.image ) {
+      let html =
+        `<div class="MessageArea">
+          <div class="Message__head">
+            <div class="Message__name">
+              ${message.user_name}
+            </div>
+            <div class="Message__date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="Message__text">
+            <p class="Form__text">
+              ${message.content}
+            </p>
+            <img class="Message__image" src="${message.image}">
+          </div>
+        </div>`
+      return html;
+    } else {
+      let html =
+      `<div class="MessageArea">
+        <div class="Message__head">
+          <div class="Message__name">
+            ${message.user_name}
+          </div>
+          <div class="Message__date">
+            ${message.created_at}
+          </div>
+        </div>
+        <div class="Message__text">
+          <p class="Form__text">
+            ${message.content}
+          </p>
+        </div>
+      </div>`
+      return html;
+    };
+  }
+
+  $('.Form').on('submit', function(e){
+    e.preventDefault();
+    let formData = new FormData(this);
+    let url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      let html = buildHTML(data);
+      $('.Chat-main__message').append(html);
+      $('.Chat-main__message').animate({ scrollTop: $('.Chat-main__message')[0].scrollHeight});
+      $('form')[0].reset();
+      $('.Submit__btn').prop('disabled', false);
+    })
+    .fail(function() {
+      alert("メッセージ送信に失敗しました");
+    })
+  });
+});
